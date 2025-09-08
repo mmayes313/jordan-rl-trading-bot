737ea79b2a5af013d38c1079d184c030269332bc
Mark Montgomery Jr <mmayes313@gmail.com>
2025-09-08 16:59:38 -0700

    Workaround for CLI errors

--- scripts/simple_interface.py
+++ scripts/simple_interface.py
@@ -1,4 +1,5 @@
 import argparse
+import subprocess
 from src.models.ppo_model import create_ppo_model
 import streamlit as st  # For dashboard
 
@@ -6,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--train', action='store_true')
 parser.add_argument('--dashboard', action='store_true')
 parser.add_argument('--tune', type=int, default=50)
+parser.add_argument('--test', action='store_true')
 args = parser.parse_args()
 
 if args.train:
@@ -18,3 +20,5 @@ elif args.tune:
     run_optimization('data/raw/eurusd_1y.csv', args.tune)
 elif args.dashboard:
     exec(open('streamlit/app.py').read())  # Run dashboard
+elif args.test:
+    subprocess.run(['python', 'tests/run_all_tests.py'])
